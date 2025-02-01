04bea2fbf3139d6f81c566041424ac739dbcd767
sanliverpool13 <kairosh.sanjar@gmail.com>
2025-02-01 13:26:25 -0500

    updated navbar theme toggle justification for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between max-w-7xl lg:w-10/12 ">
-      <div className="flex-grow" />
+    <div className="w-full flex lg:justify-end max-w-7xl lg:w-10/12 ">
+      {/* <div className="flex-grow" /> */}
       <ThemeToggle />
     </div>
   );
